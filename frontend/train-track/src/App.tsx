aec344bd8e5e723ebd3f66a81e09828ae84fc41f
Amanuel Abiy <amanuelabiy41@gmail.com>
2024-06-20 21:06:42 -0400

    Added Tailwind CSS and Shadcn/ui

--- frontend/train-track/src/App.tsx
+++ frontend/train-track/src/App.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { Button } from "./components/ui/button";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1 className="text-7xl font-bold ">App</h1>
+      <Button
+        variant="destructive"
+        size="lg"
+        onClick={() => console.log("it worked!!!")}
+      >
+        Click Me
+      </Button>
+    </div>
+  );
 }
-
 export default App;
